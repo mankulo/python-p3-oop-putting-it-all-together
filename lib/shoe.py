6214e88294ac698e61e68738260f8543a7821755
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-11-29 20:33:06 +0300

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand 
+        self._size = size
+        self._condition = "Used"
+
+    @property
+    def size (self):
+        return self._size
+
+    @size.setter
+    def size (self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+
+        else:
+            self._size = value
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def cobble(self):
+        result = "Your shoe is as good as new!"
+        self._condition = "New"
+        print(result)
+        return result
+
+    
